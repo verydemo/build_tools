9b20c4873bc96a1f976ccca76a0de009b6fefede
xiaozhang <623437847@qq.com>
2025-04-14 06:17:09 +0800

    Update make_common.py

--- scripts/core_common/make_common.py
+++ scripts/core_common/make_common.py
@@ -36,11 +36,12 @@ def make():
       if base.is_dir(toolchain):
         check_android_ndk_macos_arm(toolchain + "/prebuilt")
 
-  boost.make()
-  cef.make()
-  icu.make()
-  openssl.make()
+  #boost.make()
+  #cef.make()
+  #icu.make()
+  #openssl.make()
   v8.make()
+  exit(0)
   html2.make()
   hunspell.make(False)
   harfbuzz.make()
